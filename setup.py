75a7ad33f742c2b878d7108d057dfc4483988a75
Frederick Wettern <jaydee@thebloodyscreen.com>
2017-12-04 21:00:17 +0100

    added some stuff

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'bloodyterminal',
-  packages = ['bloodyterminal'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A little helper to structure your terminal output',
-  author = 'TheBloodyScreen',
-  author_email = 'jaydee@thebloodyscreen.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['terminal', 'console', 'print', 'debug'], # arbitrary keywords
-  classifiers = [],
+    name='bloodyterminal',
+    scripts=['bloodyterminal'],
+    version='0.1b',
+    description='A little helper to structure your terminal output',
+    author='TheBloodyScreen',
+    author_email='jaydee@thebloodyscreen.com',
+    url='https://github.com/TheBloodyScreen/bloodyterminal/'
 )
