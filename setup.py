4bd78ad0d4b4be4fabdedd2241e80803e26174ab
Frederick Wettern <jaydee@thebloodyscreen.com>
2017-12-04 19:54:09 +0100

    PyPl prep

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bloodyterminal',
+  packages = ['bloodyterminal'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A little helper to structure your terminal output',
+  author = 'TheBloodyScreen',
+  author_email = 'jaydee@thebloodyscreen.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['terminal', 'console', 'print', 'debug'], # arbitrary keywords
+  classifiers = [],
+)
